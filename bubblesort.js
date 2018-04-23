3d03579af542db976da66727be9d8f156382e5a0
Bronwyn Harris <bronwyneharris@gmail.com>
2018-04-23 13:26:54 -0400

    second commit

--- bubblesort.js
+++ bubblesort.js
@@ -1,12 +1,3 @@
-// function bubbleSort(arr){
-//   for(var i = 0; i < arr.length-1; i++){
-//     if(arr[i]>arr[i+1]){
-
-//     }
-//   }
-
-// }
-
 function bubbleSort(arr) {
  for(var i = arr.length; i > 0; i--){
       for(var j = 0; j < i; j++){
